38688af6ad32540c1ffbfa4182d8120baf9aec1b
Gmongtor <gmongtor@myuax.com>
2023-10-24 20:08:04 +0200

    Actualización

---
+++ Ejercicio2.2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+int main(){
+    int num1, num2;
+    //Operadores aritméticos
+    std::cout << "Ingresa dos números enteros: ";
+    std::cin >> num1 >> num2;
+    int suma = num1 + num2;
+    int resta = num1 - num2;
+    int multiplicacion = num1 * num2;
+    int division = num1 / num2;
+    int modulo = num1 % num2;
+    std::cout << "Suma: " << suma << std::endl;
+    std::cout << "Resta: " << resta << std::endl;
+    std::cout << "Multiplicación: " << multiplicacion << std::endl;
+    std::cout << "División: " << division << std::endl;
+    std::cout << "Módulo: " << modulo << std::endl;
+    //Operadores comparativos
+    if (num1 == num2) {
+        std::cout << "Los números son iguales." << std::endl;
+    }else{
+        std::cout << "Los números son diferentes." << std::endl;
+    }
+    if (num1 < num2) {
+        std::cout << "El primer número es menor que el segundo" << std::endl;
+    }else{
+        std::cout << "El primer número es mayor que el segundo" << std::endl;
+    }
+    //Operadores lógicos
+    bool esVerdadero = true;
+    bool esFalso = false;
+    if (esVerdadero & esFalso) {
+        std::cout << "Esto no se imprimirá, ya que la condición es falso & verdadero." << std::endl;
+    }
+    if (esVerdadero | esFalso) {
+        std::cout << "Esto no se imprimirá, ya que la condición es falso | verdadero." << std::endl;
+    }
+    if (!esFalso) {
+        std::cout << "Esto no se imprimirá, ya que la condición es !falso." << std::endl;
+    }
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/Ejercicio2.1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio2.1.dir/Ejercicio2.1.cpp.obj"
+  "CMakeFiles/Ejercicio2.1.dir/Ejercicio2.1.cpp.obj.d"
+  "Ejercicio2.1.exe"
+  "Ejercicio2.1.exe.manifest"
+  "Ejercicio2.1.pdb"
+  "libEjercicio2.1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio2.1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
